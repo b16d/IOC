30445eff8d55b10eb35faad4a3dc65fc3f87986d
b16d <albanclevy@yahoo.fr>
2024-09-09 20:43:52 +0200

    Better clean

--- src/main/java/com/acl/Main.java
+++ src/main/java/com/acl/Main.java
@@ -3,17 +3,16 @@ package com.acl;
 import com.acl.ioc.annotation.MyInject;
 import com.acl.ioc.annotation.MyService;
 
-import javax.swing.text.html.Option;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws ClassNotFoundException {
         String packageName = "com.acl.connector";
         String packageName2 = "com.acl.service";
@@ -34,25 +33,25 @@ public class Main {
 
     }
 
-    public Set<Class> findAllClassesWithAnnotation(List<String> packageList, Class annotations) throws ClassNotFoundException {
-        Set<Class> classSet = new HashSet<>();
+    public Set<Class<?>> findAllClassesWithAnnotation(List<String> packageList, Class<? extends Annotation> annotations) throws ClassNotFoundException {
+        Set<Class<?>> classSet = new HashSet<>();
         packageList.forEach(p -> classSet.addAll(findAllClassesUsingClassLoader(p)));
 
-        Set<Class> serviceClass = new HashSet<>();
-        for (Class classToCheck : classSet) {
-            if (getClassWithAnnotatiob(classToCheck, annotations)) {
+        Set<Class<?>> serviceClass = new HashSet<>();
+        for (Class<?> classToCheck : classSet) {
+            if (getClassWithAnnotation(classToCheck, annotations)) {
                 serviceClass.add(classToCheck);
             }
         }
         return serviceClass;
     }
 
-    public Set<Class> findAllAnnotatedField(List<String> packageList, Class annotations) throws ClassNotFoundException {
-        Set<Class> classSet = new HashSet<>();
+    public Set<Class<?>> findAllAnnotatedField(List<String> packageList, Class<? extends Annotation> annotations) throws ClassNotFoundException {
+        Set<Class<?>> classSet = new HashSet<>();
         packageList.forEach(p -> classSet.addAll(findAllClassesUsingClassLoader(p)));
 
-        Set<Class> annotatedFieldsClass = new HashSet<>();
-        for (Class classToCheck : classSet) {
+        Set<Class<?>> annotatedFieldsClass = new HashSet<>();
+        for (Class<?> classToCheck : classSet) {
             if (getFieldWithAnnotation(classToCheck, annotations)) {
                 annotatedFieldsClass.add(classToCheck);
             }
@@ -60,40 +59,44 @@ public class Main {
         return annotatedFieldsClass;
     }
 
-    private Set<Class> findAllClassesUsingClassLoader(String packageName) {
+    private Set<Class<?>> findAllClassesUsingClassLoader(String packageName) {
         InputStream stream = ClassLoader.getSystemClassLoader()
                 .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+
+        if (stream == null) {
+            return Collections.emptySet();
+        }
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         return reader.lines()
-               // .peek(System.out::println)
                 .filter(line -> line.endsWith(".class"))
                 .map(line -> getClass(line, packageName))
                 .collect(Collectors.toSet());
     }
 
-    private Class getClass(String className, String packageName) {
+    private Class<?> getClass(String className, String packageName) {
         try {
             return Class.forName(packageName + "."
                     + className.substring(0, className.lastIndexOf('.')));
         } catch (ClassNotFoundException e) {
-            // handle the exception
+            System.out.println("Class not found: " + e);
         }
         return null;
     }
 
-    private boolean getClassWithAnnotatiob(Class classToCheckt, Class annotatio) throws ClassNotFoundException {
+    private boolean getClassWithAnnotation(Class<?> classToCheck, Class<? extends Annotation> annotation) throws ClassNotFoundException {
         Class<?> clazz = ClassLoader.getSystemClassLoader()
-                .loadClass(classToCheckt.getName());
-        var classAnnotation = clazz.getAnnotation(annotatio);
+                .loadClass(classToCheck.getName());
+        var classAnnotation = clazz.getAnnotation(annotation);
 
         return classAnnotation != null;
     }
 
-    private boolean getFieldWithAnnotation(Class classToCheck, Class annotation) throws ClassNotFoundException {
+    private boolean getFieldWithAnnotation(Class<?> classToCheck, Class<? extends Annotation> annotation) throws ClassNotFoundException {
         Class<?> clazz = ClassLoader.getSystemClassLoader()
                 .loadClass(classToCheck.getName());
         Field[] fieldsTab = clazz.getDeclaredFields();
-        Set<Class> annotationSet = new HashSet<>();
+        Set<Class<?>> annotationSet = new HashSet<>();
         for (Field field : fieldsTab) {
             if (field.isAnnotationPresent(annotation)) {
                 annotationSet.add(classToCheck);
